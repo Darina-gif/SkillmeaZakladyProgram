7e2f150ae8a5e7dd2d897e0e0b63da2f539e2959
Darina-gif <darinkabb@gmail.com>
2025-05-27 20:06:49 +0200

    Prvni commit

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*String pozdrav = "Ahoj svete";
+
+        //Toto je pro vypsani textu
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);*/
+
+        /*int prvniCislo = 10;
+        int druheCislo = 5;
+
+        System.out.println(prvniCislo + druheCislo);
+        System.out.println(prvniCislo - druheCislo);
+        System.out.println(prvniCislo * druheCislo);
+        System.out.println(prvniCislo / druheCislo);
+
+        String pozdrav2 = "Ahoj";
+        String jmeno = "Honza";
+
+        System.out.println(pozdrav2 + " " + jmeno);*/
+
+        //Uloha: Uzivatel napise sve jmeno a program ho pozdravi
+       /* String pozdrav3 = "Ahoj, ";
+        Scanner mujScanner = new Scanner(System.in);
+        System.out.println("Zadej sve jmeno:");
+        String jmeno = mujScanner.nextLine();
+        System.out.println(pozdrav3 + jmeno);*/
+
+       //Kalkulacka
+       /* float prvniCislo;
+        float druheCislo;
+        Scanner mujScanner = new Scanner(System.in);
+
+        System.out.println("Zadej prvni cislo:");
+        prvniCislo = mujScanner.nextFloat();
+
+        System.out.println("Zadej druhe cislo:");
+        druheCislo = mujScanner.nextFloat();
+
+        System.out.println(prvniCislo + druheCislo);
+        System.out.println(prvniCislo - druheCislo);
+        System.out.println(prvniCislo * druheCislo);
+        System.out.println(prvniCislo / druheCislo);*/
+
+        double a = 0.1;
+        double b = 0.2;
+
+        double soucet = a + b;
+        System.out.println(soucet);
+
+
+
+
+    }
+}
